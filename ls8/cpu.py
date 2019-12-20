7b0c3fc67ea7e96f52f2955097ffb8e663fd2f2e
cecil john tantay <cjbtantay@gmail.com>
2019-12-20 10:57:40 -0800

    finished with sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,12 +9,17 @@ import os
 HLT = 0b00000001 
 LDI = 0b10000010 # 00000rrr iiiiiiii
 MUL = 0b10100010 # 2 operands
-# LD  = 0b10000011 00000aaa 00000bbb
-# ST  = 0b10000100 00000aaa 00000bbb
 PUSH = 0b01000101 # 00000rrr
 POP = 0b01000110 # 00000rrr
 PRN = 0b01000111 # 00000rrr
-# PRA = 0b01001000 00000rrr
+RET = 0b00010001 
+CALL = 0b01010000 # 00000rrr
+CMP = 0b10100111 # 00000aaa 00000bbb
+JEQ = 0b01010101 # 00000rrr
+JNE = 0b01010110 # 00000rrr
+JMP = 0b01010100 # 00000rrr
+ADD = 0b10100000 # 00000aaa 00000bbb
+
 
 class CPU:
     """Main CPU class."""
@@ -23,8 +28,9 @@ class CPU:
         """Construct a new CPU."""
         self.ram = {}
         self.reg = [0] * 8
-        self.__pc =  0
-        self.__sp = 5
+        self.pc =  0
+        self.sp = 5
+        self.fl = 0b00000000 # 00000LGE
 
     def load(self, filename):
         """Load a program into memory."""
@@ -86,53 +92,138 @@ class CPU:
         # print(instruction, LDI)
 
         while not halted:
-            instruction = self.ram[self.__pc]
+            instruction = self.ram[self.pc]
 
             if instruction == HLT:
                 halted = True 
             
             elif instruction == LDI:
-                register_num = self.ram[self.__pc + 1]
-                self.reg[register_num] = self.ram[self.__pc + 2]
-                self.__pc += 3
+                register_num = self.ram[self.pc + 1]
+                self.reg[register_num] = self.ram[self.pc + 2]
+                self.pc += 3
 
             elif instruction == PRN:
-                key = self.ram[self.__pc + 1]
+                key = self.ram[self.pc + 1]
                 register_num = self.reg[key]
                 print(register_num)
-                self.__pc += 2
+                self.pc += 2
             
             elif instruction == MUL:
-                registerA = self.ram[self.__pc + 1]
-                registerB = self.ram[self.__pc + 2]
+                registerA = self.ram[self.pc + 1]
+                registerB = self.ram[self.pc + 2]
                 result = self.reg[registerA] * self.reg[registerB]
                 self.reg[registerA] = result
-                self.__pc += 3
+                self.pc += 3
 
             elif instruction == PUSH:
                 # 1. Decrement the `SP`.
-                self.reg[self.__sp] -= 1
-
-                # 2. Copy the value in the given register to the address pointed to by
-                #   `SP`.
-                register_num = self.ram[self.__pc + 1]
+                self.sp -= 1
+                
+                # 2. Copy the value in the given register to the address pointed to by `SP`.
+                register_num = self.ram[self.pc + 1]
                 value = self.reg[register_num]
-                self.ram[self.reg[self.__sp]] = value
-                self.__pc += 2
+                self.ram[self.sp] = value
+                self.pc += 2
+
 
             elif instruction == POP:
                 # 1. Copy the value from the address pointed to by `SP` to the given register.
-                register_num = self.ram[self.__pc + 1]
-                value = self.ram[self.reg[self.__sp]]
+                value = self.ram[self.sp]
+                register_num = self.ram[self.pc + 1]
                 self.reg[register_num] = value
 
-                # 2. Increment `SP`.
-                self.reg[self.__sp] += 1
-                self.__pc += 2
+                # 2. Increment `SP`.      
+                self.sp += 1
+                self.pc += 2
 
+
+            elif instruction == CALL:
+            # Calls a subroutine (function) at the address stored in the register.
+                # 1. The address of the ***instruction*** _directly after_ `CALL` is pushed onto the stack. This allows us to return to where we left off when the subroutine finishes executing.
+                saved_address = self.pc + 2
+
+                # decrease sp
+                self.sp -= 1
+
+                # send saved_address to the stack in memory pointed to sp
+                self.ram[self.sp] = saved_address
+
+                # 2. The PC is set to the address stored in the given register. We jump to that location in RAM and execute the first #instruction in the subroutine. The PC can move forward or backwards from its current location.
+                register_num = self.ram[self.pc + 1]
+                self.pc = self.reg[register_num]
+            
+            elif instruction == RET:
+            # Pop the value from the top of the stack and store it in the `PC`.
+                value = self.ram[self.sp]
+                self.pc = value
+                self.sp += 1
+
+
+            elif instruction == CMP:
+                """
+                Compare the values in two registers.
+
+                * If they are equal, set the Equal `E` flag to 1, otherwise set it to 0.
+
+                * If registerA is less than registerB, set the Less-than `L` flag to 1,
+                otherwise set it to 0.
+
+                * If registerA is greater than registerB, set the Greater-than `G` flag
+                to 1, otherwise set it to 0.
+                """
+                registerA = self.ram[self.pc + 1]
+                registerB = self.ram[self.pc + 2]
+
+                if self.reg[registerA] < self.reg[registerB]:
+                    self.fl = 0b00000100
+                elif self.reg[registerA] > self.reg[registerB]:
+                    self.fl = 0b00000010
+                elif self.reg[registerA] == self.reg[registerB]:
+                    self.fl = 0b00000001
+                
+                self.pc += 3
+            
+            elif instruction == JEQ:
+                """
+                If `equal` flag is set (true), jump to the address stored in the given register.
+                """
+                if self.fl == 1:
+                    register_num = self.ram[self.pc + 1]
+                    self.pc = self.reg[register_num]
+                else:
+                    self.pc += 2
                 
 
+            elif instruction == JNE:
+                """
+                If `E` flag is clear (false, 0), jump to the address stored in the given
+                register.
+                """
+                if self.fl is not 1:
+                    register_num = self.ram[self.pc + 1]
+                    self.pc = self.reg[register_num]
+                else:
+                    self.pc += 2
+
+            elif instruction == JMP:
+                """
+                Jump to the address stored in the given register.
+
+                Set the `PC` to the address stored in the given register.       
+                """
+                register_num = self.ram[self.pc + 1]
+                self.pc = self.reg[register_num]
+
+            elif instruction == ADD:
+                # Add the value in two registers and store the result in registerA.
+                registerA = self.ram[self.pc + 1]
+                registerB = self.ram[self.pc + 2]
+                result = self.reg[registerA] + self.reg[registerB]
+                self.reg[registerA] = result
+
+                self.pc += 3
+
             else:
-                print(f"Uknown instruction at index {self.__pc}")
+                print(f"Uknown instruction at index {self.pc}")
 
 
